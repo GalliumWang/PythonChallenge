67fd27fa8e89efb7bc22ba5c0ac5db3045f92f6d
Gallium Wang <galliumwang199@gmail.com>
2020-10-14 19:58:34 +0800

    level 2 completed

---
+++ Level2.py
@@ -0,0 +1,11 @@
+def char_frequency(str1):
+    dict = {}
+    for n in str1:
+        keys = dict.keys()
+        if n in keys:
+            dict[n] += 1
+        else:
+            dict[n] = 1
+    return dict
+
+print(char_frequency(open("./Level2Text.txt","r").read()))
