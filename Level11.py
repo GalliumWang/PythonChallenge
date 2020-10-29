882a64cb22b2d0765a29e1181d1223cf7b0eb83d
Gallium Wang <galliumwang199@gmail.com>
2020-10-30 01:16:18 +0800

    update level 11

--- Level11.py
+++ Level11.py
@@ -1,8 +1,18 @@
-import urllib.request
+from PIL import Image
 
-url="http://www.pythonchallenge.com/pc/return/cave.jpg"
+im = Image.open('cave.jpg')
 
-urllib.request.urlretrieve (url, url.split('/')[-1])
+(w, h) = im.size#(640,480)
 
+even = Image.new('RGB', (w // 2, h // 2))
+odd = Image.new('RGB', (w // 2, h // 2))
 
-#open(url.split('/')[-1], 'wb').write(r.content)
+for i in range(w):
+    for j in range(h):
+        p = im.getpixel((i,j))
+        if (i+j)%2 == 1:
+            odd.putpixel((i // 2, j // 2), p)
+        else:
+            even.putpixel((i // 2, j // 2), p)
+even.save('even.jpg')
+odd.save('odd.jpg')
